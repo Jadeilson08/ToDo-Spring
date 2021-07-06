f43392d0fd23e63952933375e2467cc6a1300125
jadeilson <jadeilson08@gmail.com>
2021-07-05 20:35:55 -0400

    #feature - basicEntity

--- src/main/java/com/todo/todo/model/BasicEntity.java
+++ src/main/java/com/todo/todo/model/BasicEntity.java
@@ -1,5 +1,7 @@
 package com.todo.todo.model;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.Objects;
--- src/main/java/com/todo/todo/model/Categoria.java
+++ src/main/java/com/todo/todo/model/Categoria.java
@@ -1,8 +1,30 @@
 package com.todo.todo.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
 public class Categoria extends BasicEntity {
 
+    @Column(name = "titulo", nullable = false, length = 30)
+    private String titulo;
+
+    @Column(name = "descricao", nullable = true, length = 255)
+    private String descricao;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
